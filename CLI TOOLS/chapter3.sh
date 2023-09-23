8d490cd5a5cdb0f48c05d11b623a63e176f48b44
Rustam Rakhimov <83468895+Rustam64@users.noreply.github.com>
2023-09-23 18:04:58 +0900

    Create chapter3.sh

---
+++ CLI TOOLS/chapter3.sh
@@ -0,0 +1,7 @@
+#CHAPTER 3: Understanding nodes
+
+ros2 run turtlesim turtlesim_node
+#Here the 'turtlesim' is the package and 'turtlesim_node' is the executable.
+
+ros2 node list
+#Run this command in a new terminal to see active nodes.
